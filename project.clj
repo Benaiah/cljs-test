284e462f9aa70b9c729b98c366bd418022910e85
Benaiah Mischenko <benaiah@mischenko.com>
2015-01-28 13:15:16 -0800

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,15 +1,10 @@
-(defproject cljs-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "0.0-2740"]]
+(defproject lein-cljsbuild-example "1.2.3"
+  :dependencies [[org.clojure/clojurescript "0.0-2727"]]
   :plugins [[lein-cljsbuild "1.0.4"]]
   :cljsbuild {
-              :builds {
-                       :main {
-                              :source-path "src/cljs"
-                              :compiler {
-                                         :output-to "main.js"
-                                         :optimizations :simple
-                                         :pretty-print true}}}})
+              :builds [{
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :output-to "bin/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
